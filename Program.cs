22c672486c0c5ec935755db87e0a6e15ae4fb29e
Harol Cisneros <harolcsnrs@gmail.com>
2024-10-19 18:23:47 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using Lista_Brindis;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            lista lista = new lista();
+
+            int opcion=0;
+
+            while(opcion != 8)
+            {
+                Console.WriteLine("1.- Insertar Nodo");
+                Console.WriteLine("2.- Imprimir Tamaño");
+                Console.WriteLine("3.- Buscar Nodo");
+                Console.WriteLine("4.- Borrar Nodo");
+                Console.WriteLine("5.- Modificar Nodo");
+                Console.WriteLine("6.- Buscar Valor");
+                Console.WriteLine("7.- Imprimir Lista");
+                Console.WriteLine("8.- Salir");
+                opcion=Convert.ToInt32(Console.ReadLine()); 
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Usted eligió la opción 1");
+                        Console.WriteLine("Dame valor del Nodo");
+                        int num=Convert.ToInt32(Console.ReadLine());
+                        lista.Add(num);
+                        break;
+
+                        case 2:
+                        Console.WriteLine("Usted eligió la opción 2");
+                        lista.count();
+                        break; 
+
+                    case 3:
+                        Console.WriteLine("Usted eligió la opción 3");
+                        Console.WriteLine("Dame posición");
+                        int posi=Convert.ToInt32(Console.ReadLine());
+                        lista.Find(posi);
+                        break;
+
+                        case 4:
+                        Console.WriteLine("Usted eligió la opción 4");
+
+                        break;
+
+                        case 5:
+                        Console.WriteLine("Usted eligió la opción 5");
+                        Console.WriteLine("Dame posición");
+                        int pos=Convert.ToInt32(Console.ReadLine());
+                        lista.Changer(pos);
+                        break;
+
+                        case 6:
+                        Console.WriteLine("Usted eligió la opción 6");
+                        Console.WriteLine("Dame valor");
+                        int numero = Convert.ToInt32(Console.ReadLine());
+                        lista.findValue(numero);
+                        break;
+
+                        case 7:
+                        Console.WriteLine("Usted eligió la opción 7");
+
+                        lista.Print();
+                        break;
+
+                        case 8:
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción Incorrecta");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ lista.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_Brindis
+{
+    internal class lista
+    {
+        private nodo inicio;
+
+        public lista()
+        {
+            inicio = null;
+        }
+
+        public void Add(int num)
+        {
+            nodo nuevo = new nodo(num);
+
+            if (inicio == null)
+            {
+                inicio= nuevo;
+            }
+            else
+            {
+                nodo act=inicio;
+                while (act.Sig != null)
+                {
+                    act = act.Sig;
+                }
+                act.Sig=nuevo;
+            }
+        }
+
+        public void count(int n=0)
+        {
+            nodo act;
+            act= inicio;
+            
+
+            if (act == null)
+            {
+                Console.WriteLine("La lista esta vacía");
+                Console.WriteLine(n);
+            }
+            else
+            {
+                n++;
+                while(act.Sig != null)
+                {
+                    act= act.Sig;
+                    
+                    n++;
+                }
+                Console.WriteLine($"La lista tiene {n} nodo"); 
+            }
+           
+        }
+
+        public void Find(int posi)
+        {
+            nodo act;
+            act = inicio;
+            int i = 1;
+            if (act==null)
+            {
+                Console.WriteLine("La lista esta vacía");
+            }
+            while (act != null && i < posi)
+            {
+                act=act.Sig;
+                i++;
+            }
+            if(act==null)
+            {
+                Console.WriteLine($"No hay nodo en la posición {posi}");
+            }
+            else
+            {
+                Console.WriteLine($"El valor es {act.Valor}");
+            }
+            
+        }
+
+        public void findValue(int numero)
+        {
+            nodo act;
+            act = inicio;
+            int i = 0;
+            bool encontrado = false;
+
+            if (act == null)
+            {
+                Console.WriteLine("La lista esta vacía");
+            }
+            else
+            {
+
+                while (act != null && encontrado != true)
+                {
+
+                    i++;
+                    if (act.Valor == numero)
+                    {
+
+                        Console.WriteLine($"La posición es {i}");
+                        encontrado = true;
+                    }
+                   
+                    act = act.Sig;
+                }
+            }
+        }
+
+        public void Changer(int pos)
+        {
+            nodo act;
+            act= inicio;
+            int i = 1;
+            int nuevoValor;
+            
+            if(act == null)
+            {
+                Console.WriteLine("La lista esta vacía");
+            }
+            while (act != null && i < pos)
+            {
+                act = act.Sig;
+                i++;
+            }
+            if (act == null)
+            {
+                Console.WriteLine($"No hay nodo en la posición {pos}");
+            }
+            else
+            {
+                Console.WriteLine("Ingrese el nuevo valor para nodo");
+                nuevoValor=Convert.ToInt32(Console.ReadLine());
+
+                act.Valor = nuevoValor;
+                Console.WriteLine($"El valor del nodo en la posición {pos} ha sido modificado a: {nuevoValor}");
+
+            }
+
+        }
+        public void Print()
+        {
+            nodo act;
+            act=inicio;
+
+            if(act == null)
+            {
+                Console.WriteLine("La lista esta vacía");
+            }
+            else
+            {
+                while(act!=null)
+                {
+                    Console.WriteLine($"{act.Valor}");
+                    act=act.Sig;
+                }
+            }
+        }
+
+    }
+}
